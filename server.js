167a0ee34342e5677bb1739f10b9a6886ef1db93
Josselin Milon <josselinmilon@macbook-pro-de-josselin-2.home>
2022-07-09 18:09:57 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+app.post("/which-gen", async (req, res) => {
+  try {
+    //Parse the search name
+    const searchTerms = req.fields.search
+      .split(" ")
+      .map((item) => {
+        return item[0].toUpperCase() + item.substring(1);
+      })
+      .join("_");
+    //Load the text scrapped from the page
+    const response = await axios.get(
+      `https://wikipedia.org/wiki/${searchTerms}`
+    );
+    const $ = cheerio.load(response.data);
+
+    //Set the response object
+    const searchPerson = {};
+
+    //Get the picture
+    const searchPersonPicRoute = $(".infobox-image").find("a").attr("href");
+    const searchPersonPicSrc = `https://wikipedia.org/${searchPersonPicRoute}`;
+    searchPerson.src = searchPersonPicSrc;
+
+    //Get the wikipedia name
+    searchPerson.name = $(".infobox-above").text();
+
+    //Get the birth date and the gen
+    const searchedPersonBday = new Date($(".bday").text());
+    const searchedPersonBirthYear = searchedPersonBday.getFullYear();
+    searchPerson.birthYear = searchedPersonBirthYear;
+    searchPerson.gen =
+      searchedPersonBirthYear < 1965
+        ? "Boomer"
+        : searchedPersonBirthYear < 1984
+        ? "Gen X"
+        : searchedPersonBirthYear < 1997
+        ? "Gen Y"
+        : "Gen Z";
+
+    //Return response
+    res.status(200).json(searchPerson);
+  } catch (error) {
+    res.status(400).json({ message: "search failed" });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server has started...");
+});
