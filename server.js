8617fd8000d0c2094523f7e768d51d1ba69b3761
Josselin Milon <josselinmilon@macbook-pro-de-josselin.home>
2022-07-13 10:13:16 -0400

    image scraping from src

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Packages
 const cheerio = require("cheerio");
 const axios = require("axios");
 const express = require("express");
@@ -5,10 +6,12 @@ const formidable = require("express-formidable");
 const cors = require("cors");
 require("dotenv").config();
 
+//Server setup + package mounting
 const app = express();
 app.use(formidable());
 app.use(cors());
 
+//Routes
 app.post("/which-gen", async (req, res) => {
   try {
     //Parse the search name
@@ -28,17 +31,18 @@ app.post("/which-gen", async (req, res) => {
     const searchPerson = {};
 
     //Get the picture
-    const searchPersonPicRoute = $(".infobox-image").find("a").attr("href");
-    const searchPersonPicSrc = `https://wikipedia.org/${searchPersonPicRoute}`;
-    searchPerson.src = searchPersonPicSrc;
+    const searchPersonPicRoute = $(".infobox-image").find("img").attr("src");
+    searchPerson.src = `https:${searchPersonPicRoute}`;
 
     //Get the wikipedia name
     searchPerson.name = $(".infobox-above").text();
 
-    //Get the birth date and the gen
+    //Get the birth date
     const searchedPersonBday = new Date($(".bday").text());
     const searchedPersonBirthYear = searchedPersonBday.getFullYear();
     searchPerson.birthYear = searchedPersonBirthYear;
+
+    //Get the gen (based on DoB)
     searchPerson.gen =
       searchedPersonBirthYear < 1965
         ? "Boomer"
